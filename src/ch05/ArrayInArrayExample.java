7bdd8e25fb6398d20337d086c34a00eb069dea41
Administrator <gim1039@naver.com>
2021-10-01 16:32:47 +0900

    211001

---
+++ src/ch05/ArrayInArrayExample.java
@@ -0,0 +1,19 @@
+package ch05;
+
+public class ArrayInArrayExample {
+    public static void main(String[] args) {;
+        int[][] arr = new int[4][4];
+
+        System.out.println(arr.length);
+        System.out.println(arr[0].length);
+        System.out.println(arr[1].length);
+        //              2
+        for(int i=0; i<arr.length; i++) {
+             //                 3
+            for(int z=0; z< arr[i].length; z++) {
+                System.out.printf("(%d, %d) : %d\t", i, z, arr[i][z]);
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ src/ch05/MethodExample2.java
@@ -0,0 +1,13 @@
+package ch05;
+
+public class MethodExample2 {
+    public static void main(String[] args) {
+        // input 0, output 0 (비void형)
+        int result = sum(100, 200);
+        System.out.println("sum: " + result);
+    }
+
+    public static int sum(int n1, int n2) {
+        return n1 + n2; // void 외에는 무조건 return을 적어주어야 한다.
+    }
+}
